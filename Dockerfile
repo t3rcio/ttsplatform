1fbfbdc3d6579600a11432b9f902981a7d8d5502
Tercio <tercio@torabit.com>
2022-04-06 15:57:02 -0300

    Estruturando...

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+# syntax=docker/dockerfile:1
+FROM python:3.9
+ENV PYTHONUNBUFFERED=1
+RUN apt-get update && apt-get install -y supervisor
+RUN mkdir -p /var/log/supervisor
+WORKDIR /code
+COPY requirements.txt /code/
+RUN pip install -r requirements.txt
+COPY . /code/
+
+COPY ./deploy/supervisord.conf /etc/supervisor/conf.d/supervisord.conf
+
+RUN chmod +x start.sh
+RUN /code/start.sh
+
+RUN supervisord -c /etc/supervisor/conf.d/supervisord.conf &
---
+++ worker.py
@@ -0,0 +1,47 @@
+import sys
+import os
+import django
+import traceback
+import json
+import importlib
+import time
+
+__ENV__ = os.environ.get("ENVIRONMENT","development")
+if __ENV__ == 'development':    
+    sys.path.insert(0, '/home/tercio/Projetos/ttsplatform')
+    sys.path.insert(1, '/home/tercio/Projetos/ttsplatform/ttsapp')
+elif __ENV__ == 'production':
+    sys.path.insert(0, '/code')
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "ttsapp.settings"
+
+django.setup()
+from django.conf import settings
+from tasker import init_broker, logger
+
+connection, channel = init_broker()
+
+def main():
+    queue = settings.TASKER['default_broker']['queue']
+    def callback(ch, method, properties, body):
+        task_json = json.loads(body)
+        task_module = '{}'.format(task_json['module'])
+        _module = importlib.import_module(task_module)
+        args = json.loads(task_json['args'])
+        kwargs = json.loads(task_json['kwargs'])
+        _function = getattr(_module, task_json['name'])
+        _function(*args, **kwargs)
+        msg = "Running task... {}".format(kwargs)
+        logger(msg, 'success', 'consumer')
+
+    channel.queue_declare(queue=queue)
+    channel.basic_consume(queue=queue, auto_ack=True, on_message_callback=callback)
+    channel.start_consuming()
+    time.sleep(20)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except:
+        msg = traceback.print_exc()
+        logger(msg,'error','consumer')
