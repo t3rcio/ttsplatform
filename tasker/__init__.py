e45fccd39b8e87420cd8e6a8504b94c0847016c3
Tercio <tercio@torabit.com>
2022-07-09 23:34:23 -0300

    WIP

--- tasker/__init__.py
+++ tasker/__init__.py
@@ -2,14 +2,19 @@
 from django.conf import settings
 from datetime import datetime
 
+import sys
 import pika
 import traceback
 
-def logger(msg:str, type:str, source='tasker') -> None:    
-    _file = '{}/{}_{}.log'.format(settings.APP_LOGPATH,datetime.now().strftime('%Y_%m_%d'), source)
-    _type = '[{}]'.format(type.upper())
-    with open(_file, 'a') as logfile:
-        logfile.write('{} - {}  - {}\n'.format(datetime.now(), _type, msg))
+def logger(msg:str, type:str, source='tasker') -> None:
+    try:
+        _file = '{}/{}_{}.log'.format(settings.APP_LOGPATH,datetime.now().strftime('%Y_%m_%d'), source)
+        _type = '[{}]'.format(type.upper())
+        with open(_file, 'a') as logfile:
+            logfile.write('{} - {}  - {}\n'.format(datetime.now(), _type, msg))
+    except:
+        traceback.print_exc(file=sys.stderr)
+        
 
 
 def init_broker() -> tuple:
@@ -31,6 +36,7 @@ def init_broker() -> tuple:
         channel.exchange_declare(exchange=exchange, exchange_type='fanout', durable=True)
         channel.queue_bind(exchange=exchange, queue=settings.TASKER['default_broker']['queue'])
     except ValueError:
+        traceback.print_exc(file=sys.stderr)
         msg = traceback.print_exc()
         logger(msg=msg, type='error')
     return connection, channel
